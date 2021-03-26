a13a9d1c9411951d1582ea7885567ff669ae66e1
david112019 <david.cm3030@gmail.com>
2021-03-26 16:07:26 -0300

    starting to add structure to receive API

---
+++ MVP_Atitude/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+
+  <h1>{{ mvp }}</h1>
+
+  <ul>
+    <li v-for="foto of fotos">
+      <img :src="foto.url" :alt="foto.titulo">
+    </li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      mvp: 'MVP Atitude',
+      fotos:[{
+        url:'https://img.elo7.com.br/product/main/2C475C8/teste-teste.jpg',
+        titulo: 'teste'
+      },
+      {
+        url:'https://img.elo7.com.br/product/main/2C475C8/teste-teste.jpg',
+        titulo: 'teste2'
+      }
+      ],
+    }
+
+  }
+}
+</script>
+
+<style>
+</style>
